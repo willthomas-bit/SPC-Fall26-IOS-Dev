743110bc6b552be64d1997b48cbc0e7233e0c9ab
Will Thomas <will.thomas4444@gmail.com>
2026-08-23 20:49:35 -0400

    Initial Commit

---
+++ GitIgnore/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitIgnore
+//
+//  Created by Will Thomas on 8/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitIgnore/GitIgnoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitIgnoreApp.swift
+//  GitIgnore
+//
+//  Created by Will Thomas on 8/23/26.
+//
+
+import SwiftUI
+
+@main
+struct GitIgnoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
